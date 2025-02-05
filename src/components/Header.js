8c529e6ba899325761ad55838bb1d37bcc0bf101
Meghana Inaganti <meghanainaganti@gmail.com>
2025-02-04 22:26:23 -0600

    edited

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Header = () => {
     return (
         <div class="row">
-            <img src="../../images/japaneserestaurant.jpg" alt="taco bell logo" class="logo"/>
+            <img src="../../images/japaneserestaurant.jpg" alt="restaurant logo" class="logo"/>
             <p class="firsttext">Authentic Japanese Cuisine</p>
             <p class="secondtext">Savor the Art of Flavor</p>
         </div>
